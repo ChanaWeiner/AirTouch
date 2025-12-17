2a2aaf92866d9b904ccd4c818f99ebf0f2fb5e3a
ChanaWeiner <h85050909@gmail.com>
2025-12-17 14:26:00 +0200

    refactor: reorganize imports and add SSL context configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,18 @@
 # app/main.py
+import ssl
+import os
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from .api import gemini_router
 
+from app.api import gemini_router
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# מבטל אימות בספריית requests
+os.environ['CURL_CA_BUNDLE'] = ""
+os.environ['PYTHONHTTPSVERIFY'] = '0'
 app = FastAPI(title="AirTouch API")
 
 app.add_middleware(
@@ -14,7 +23,13 @@ app.add_middleware(
 )
 
 app.include_router(gemini_router.router)
-
+# app.include_router(testings.router)
 @app.get("/")
 def read_root():
     return {"message": "AirTouch Server is Running! (Clean Architecture)"}
+
+
+# בסוף קובץ app/main.py
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
